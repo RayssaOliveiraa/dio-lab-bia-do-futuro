27015ec13d5cf897cd306b173076c5cc8c1dfe46
Rayssa Oliveira <87939812+RayssaOliveiraa@users.noreply.github.com>
2026-03-18 11:27:31 -0300

    Create app.py
    Adicionando o educador financeiro com o streamlit.

---
+++ src/app.py
@@ -0,0 +1,66 @@
+import json
+import pandas as pd
+import requests
+import streamlit as st
+
+
+# ============ CONFIGURAÇÃO ============
+OLLAMA_URL = "http://localhost:11434/api/generate"
+MODELO = "gpt-oss"
+
+# =========== CARREGAR DADOS ===========
+perfil = json.load(open('./data/perfil_investidor.json'))
+transacoes = pd.read_csv('./data/transacoes.csv')
+historico = pd.read_csv('./data/historico_atendimento.csv')
+produtos = json.load(open('./data/produtos_financeiros.json'))
+
+# ============ MONTAR CONTEXTO ============
+contexto = f"""
+CLIENTE: {perfil['nome']}, {perfil['idade']} anos, perfil {perfil['perfil_investidor']}
+OBJETIVO: {perfil['objetivo_principal']}
+PATRIMÔNIO: R$ {perfil['patrimonio_total']} | RESERVA: R$ {perfil['reserva_emergencia_atual']}
+
+TRANSAÇÕES RECENTES:
+{transacoes.to_string(index=False)}
+
+ATENDIMENTOS ANTERIORES:
+{historico.to_string(index=False)}
+
+PRODUTOS DISPONÍVEIS:
+{json.dumps(produtos, indent=2, ensure_ascii=False)}
+"""
+# ============ SYSTEM PROMPT ============
+SYSTEM_PROMPT = """Você é um educador financeiro um educador  didático e amigável e jovem chamadado " Guard Financeiro",  inteligente especializado em Finanças. 
+Seu objetivo é Ensinar conceitos de finanças pessoais de forma simples, usando os dados do cliente como exemplo praticos.
+
+REGRAS:
+-  Nunca recomendar investimentos especificos, apenas o seu funcionamento.
+- Use os dados fornecidos para da exemplos personalizados.
+- Sua linguagem é simples e clara, como se tivesse explicando a um amigo, e para quem tem pouco ou nenhum entendimento do assunto.
+- Se não souber algo, admita : " Não tenha essa informações, mas posso explicar .."
+- Sempre pergunte se o cliente entendeu oque você ta explicando.
+- Responda  de forma direta e sucinta, com no máximo 3 paragrafos. 
+- JAMAIS responda a perguntas fora do tema ensino de finanças pessoais. 
+  Quando ocorrer, responda lembrando o seu papel de educador financeiro;
+"""
+
+# ============ CHAMAR OLLAMA ============
+def perguntar(msg):
+    prompt = f"""
+    {SYSTEM_PROMPT}
+
+    CONTEXTO DO CLIENTE:
+    {contexto}
+
+    Pergunta: {msg}"""
+
+    r = requests.post(OLLAMA_URL, json={"model": MODELO, "prompt": prompt, "stream": False})
+    return r.json()['response']
+
+# =========== INTERFACE ===========
+st.title("🎓 Guard Financeiro, Seu Educador Financeiro")
+
+if pergunta := st.chat_input("Sua dúvida sobre finanças..."):
+    st.chat_message("user").write(pergunta)
+    with st.spinner("..."):
+        st.chat_message("assistant").write(perguntar(pergunta))
